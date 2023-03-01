1e3a542c737123b8add5ba9edf87a6980cf7731d
student <karolma02@gmail.com>
2023-03-01 16:09:18 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        Point p1 = new Point(2, 3);
+
+        System.out.println(p1.x);
+        // locale.ENGHLISH - format angielski dodający kropki
+        String s = String.format(Locale.ENGLISH,"<line y1=\"%f\" x1=\"%f\" y1=\"%f\" x1=\"%f\" stroke=\"green\" />");
+        System.out.println(s);
+
+        int array[] = new int[4];
+        array[1] = 2;
+
+
     }
+
+
 }
---
+++ src/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+    public final double x; //ostateczne = final ~ stała od razu musi być w konstruktorze lub od razu zdeiniowana
+    public double y;
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+}
